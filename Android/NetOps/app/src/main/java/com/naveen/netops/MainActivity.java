fd778a5afecb89b0639553288a22a6ed02f265cf
Naveen Doraiswamy <naveend77@users.noreply.github.com>
2016-03-29 03:54:22 -0700

    Display list of networks in a ListView

--- Android/NetOps/app/src/main/java/com/naveen/netops/MainActivity.java
+++ Android/NetOps/app/src/main/java/com/naveen/netops/MainActivity.java
@@ -1,13 +1,20 @@
 package com.naveen.netops;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,13 +27,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        final ListView networkListView = (ListView)findViewById(R.id.listView_networks);
+        final TextView chooseNetworkTextView = (TextView)findViewById(R.id.textView_choose_network);
+        chooseNetworkTextView.setVisibility(View.INVISIBLE);
+
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                List<String> networkList = new ArrayList<String>();
+                List<ScanResult> results = wifi.getScanResults();
+                for (ScanResult result : results) {
+                    networkList.add(result.SSID);
+                }
+                chooseNetworkTextView.setVisibility(View.VISIBLE);
+                networkListView.setAdapter(new ArrayAdapter<String>(
+                        getApplicationContext(), R.layout.row_network, networkList));
+            }
+        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
     }
 
-    public void chooseNetwork(View view) {
+    public void showNetworks(View view) {
         wifi.startScan();
-        List<ScanResult> results = wifi.getScanResults();
-        for (ScanResult result : results) {
-            Toast.makeText(this, result.SSID,  Toast.LENGTH_SHORT).show();
-        }
     }
 }
